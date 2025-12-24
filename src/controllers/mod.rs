a6f383186125c645e4399fcee5e3c897b550f9fa
humanely <prabhatkr@gmail.com>
2025-12-24 14:05:10 -0500

    feat: Add image sitemap support (Task 3)
    - New /image-sitemap.xml endpoint
    - Extracts featured_image from pages
    - XML sitemap with image:image elements
    - APP_BASE_URL configurable
    
    SEO Impact: Image search visibility

--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -1,6 +1,7 @@
-pub mod module_controllers;
-pub mod page_controllers;
 pub mod category_controllers;
-pub mod user_controllers;
-pub mod sitemap_controller;
+pub mod page_controllers;
+pub mod module_controllers;
+pub mod user_controller;
 pub mod robots_controller;
+pub mod sitemap_controller;
+pub mod image_sitemap_controller;
